b9f24afff6b7920a6e50a770c3da5cea7aad4e2d
Artem Kozin <akozin@car10s1.42barcelona.com>
2025-01-24 16:00:05 +0100

    clickable, exploding bubbles

--- Bubble.gd
+++ Bubble.gd
@@ -3,10 +3,15 @@ extends CharacterBody2D
 var rng = RandomNumberGenerator.new()
 var r : float
 var i : int = 0
+var killable : bool = false
+var dying : bool = false
 @onready var texture_pool : Array = [
 	load("res://resources/BubbleSmall.png"),
 	load("res://resources/BubbleMed.png"),
 	load("res://resources/BubbleBig.png")]
+
+@onready var explosion = preload("res://explosion.tscn")
+var explosion_instance
 
 func _ready():
 	r = rng.randf_range(2.0, 3.0)
@@ -16,14 +21,32 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	if killable and !dying:
+		if Input.is_action_just_pressed("LMB"):
+			killable = false
+			dying = true
+			$Sprite2D.queue_free()
+			explosion_instance = explosion.instantiate()
+			add_child(explosion_instance)
 	move_and_slide()
 	pass
 
 func _on_timer_timeout():
-	if (i >= 2):
-		return
-	i += 1
-	r = rng.randf_range(2.0, 3.0)
-	$Sprite2D.texture = texture_pool[i]
-	$Timer.wait_time = r
-	$Timer.start()
+	if !dying:
+		if (i >= 2):
+			return
+		i += 1
+		r = rng.randf_range(2.0, 3.0)
+		$Sprite2D.texture = texture_pool[i]
+		$Timer.wait_time = r
+		$Timer.start()
+
+
+func _on_area_2d_mouse_entered():
+	if !dying:
+		killable = true
+
+
+func _on_area_2d_mouse_exited():
+	if !dying:
+		killable = false
