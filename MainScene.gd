9d7ec0a7b9137d69a77296ec17189f9015037b89
Artem Kozin <akozin@car10s1.42barcelona.com>
2025-01-24 14:38:18 +0100

    before branching

--- MainScene.gd
+++ MainScene.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+var rng = RandomNumberGenerator.new()
+var r : float
 @onready var tio = preload("res://tio.tscn")
 var instance_of_tio
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +14,11 @@ func _process(delta):
 
 func _on_timer_timeout():
 	instance_of_tio = tio.instantiate()
-	instance_of_tio.velocity.x = 20
+	instance_of_tio.velocity.x = 50
+	instance_of_tio.global_position = Vector2(-250, 120)
 	add_child(instance_of_tio)
+	r = rng.randf_range(0.3, 1.3)
+	$Timer.wait_time = r
 	$Timer.start()
 
 func _on_killzone_tio_left_body_entered(body):
