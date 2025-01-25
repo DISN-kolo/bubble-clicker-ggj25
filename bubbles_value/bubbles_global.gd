5e195a83d6f58be4b9474cb5132464248be93766
Molasz <martiolasz@gmail.com>
2025-01-25 13:16:38 +0100

    ui shop setup

---
+++ bubbles_value/bubbles_global.gd
@@ -0,0 +1,116 @@
+extends Node
+
+signal bubbles_updated;
+
+const upgrades = [
+	{
+		"name": "Tios",
+		"quantity": 1,
+		"img": "single_tio.png",
+		"price": "1",
+	},
+]
+
+var bubbles_value: Array = []:
+	set(value):
+		bubbles_value = value;
+		bubbles_updated.emit();
+
+var passive: String = "0";
+
+func _ready():
+	bubbles_value = arrayBubbles("0");
+
+func arrayBubbles(num: String):
+	var arr = [];
+	
+	while num != "":
+		if num.length() < 3:
+			arr.append(int (num));
+			break;
+		arr.append(int (num.substr(num.length() - 3, 3)));
+		num = num.substr(0, num.length() - 3);
+	return arr;
+	
+func add(num : Array):
+	var result = [];
+	var carry = 0;
+ 
+	while num.size() > 0 or bubbles_value.size() > 0:
+		var sum = 0;
+ 
+		if num.size() > 0:
+			sum += num.pop_front();
+ 
+		if bubbles_value.size() > 0:
+			sum += bubbles_value.pop_front();
+ 
+		sum += carry;
+		carry = 0;
+ 
+		result.append(arrayBubbles(str(sum))[0]);
+ 
+		if arrayBubbles(str(sum)).size() > 1:
+			carry = arrayBubbles(str(sum))[1];
+ 
+	if carry != 0:
+		result.append(carry);
+ 
+	bubbles_value = result;
+	
+func subtract(num):
+	var result = [];
+	var borrow = 0;
+ 
+	while num.size() > 0 or bubbles_value.size() > 0:
+		var diff= 0;
+ 
+		if bubbles_value.size() > 0:
+			diff += bubbles_value.pop_front();
+ 
+		if num.size() > 0:
+			diff -= num.pop_front();
+ 
+		diff -= borrow;
+ 
+		if diff < 0:
+			diff += 1000;
+			borrow = 1;
+		else:
+			borrow = 0;
+ 
+		result.append(arrayBubbles(str(diff))[0]);
+	bubbles_value = result;
+
+func compare(num: String):
+	var numStr = displayBubbles();
+	var i = 0;
+	
+	num = num.pad_zeros(3);
+	numStr = numStr.pad_zeros(3);
+	if (num.length() != numStr.length()):
+		return num.length() < numStr.length();
+	while i < numStr.length():
+		if (num[i] != numStr[i]):
+			return num[i] < numStr[i];
+		i += 1;
+	return true;
+
+func addBubbles(num: String):
+	add(arrayBubbles(num));
+	
+func subtractBubbles(num: String):
+	subtract(arrayBubbles(num));
+
+func displayBubbles():
+	var tmp = bubbles_value.duplicate();
+	var nStr = "";
+	
+	while tmp:
+		nStr += str(tmp.pop_back()).pad_zeros(3);
+	
+	for i in range(nStr.length()):
+		if nStr[i] != '0':
+			nStr = nStr.substr(i,-1)
+			break;
+	return nStr;
--- ui_shop/ui_shop.gd
+++ ui_shop/ui_shop.gd
@@ -1,16 +1,19 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var UPGRADE = preload("res://ui_shop/upgrade.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready():
+	for upgrade in BubblesGlobal.upgrades:
+		var control = UPGRADE.instantiate();
+		control.upgrade_name = upgrade.name;
+		control.upgrade_price = upgrade.price;
+		control.upgrade_quantity = upgrade.quantity;
+		if (ResourceLoader.exists("res://resources/" + upgrade.img)):
+			control.upgrade_img = ResourceLoader.load("res://resources/" + upgrade.img);
+		$Control/Upgrades.add_child(control);
 
 func _on_shopping_cart_button_up():
 	%Control.visible = true;
-
 
 func _on_close_button_button_up():
 	%Control.visible = false;
